607ffc61906df7c0c13f2cf272477031d0e107f5
JesusMLares <twinlares2@hotmail.com>
2024-05-04 14:15:40 -0700

    Added cat pictures and working on places/index

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'https://github.com/JesusMLares/Rest-Rant/blob/main/img/kitten1.jpg'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'https://github.com/JesusMLares/Rest-Rant/blob/main/img/kitten2.jpg'
+      }]
+      
+    res.render('places/index', {places})
 })
 
 module.exports = router
