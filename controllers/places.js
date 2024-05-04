d1651998ebf464246077314c47df416621492f6d
JesusMLares <twinlares2@hotmail.com>
2024-05-04 14:21:44 -0700

    Img links work now

--- controllers/places.js
+++ controllers/places.js
@@ -6,13 +6,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'https://github.com/JesusMLares/Rest-Rant/blob/main/img/kitten1.jpg'
+        pic: 'https://github.com/JesusMLares/Rest-Rant/blob/main/img/kitten1.jpg?raw=true'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'https://github.com/JesusMLares/Rest-Rant/blob/main/img/kitten2.jpg'
+        pic: 'https://github.com/JesusMLares/Rest-Rant/blob/main/img/kitten2.jpg?raw=true'
       }]
       
     res.render('places/index', {places})
