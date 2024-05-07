94df2bd0323e4b56add962d0f2c3f289657be1e1
JesusMLares <twinlares2@hotmail.com>
2024-05-06 20:20:21 -0700

    Adding Bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -8,6 +8,10 @@ function home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <img src="/images/pink.jpg" alt="pink beverage by Alex Lvrs" />
+        <div>
+          Photo by <a href="https://unsplash.com/photos/pink-beverage-on-glass-with-blackberries-and-pink-flower-on-top-sBDJADrnsbY">Alex Lvrs</a>
+        </div>
         <a href="/places">
           <button className="btn-primary">Places Page</button>
         </a>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,25 +3,32 @@ const React = require('react');
 const Def = require('../default')
 
 //Create index page using default page
-function index(data){
-    //Uses places object in places.js
+function index (data) {
     let placesFormatted = data.places.map((place) => {
-        return(
-            <div>
-                <h2>{place.name}</h2>
-                <img src={place.pic} alt={place.name} />
-            </div>
+        return (
+          <div className="col-sm-6">
+            <h2>{place.name}</h2>
+            <p className="text-center">
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p className="text-center">
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
         )
-    })
-
-    return(
-        <Def>
-            <main>
-                <h1>PLACES INDEX PAGE</h1>
+      })
+      
+    return (
+      <Def>
+          <main>
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
                 {placesFormatted}
-            </main>
-        </Def>
+              </div>
+          </main>
+      </Def>
     )
-};
+  }
 
 module.exports = index;
