683b7bda8b82b8c11c6ff16f8052ae881bdf2284
JesusMLares <twinlares2@hotmail.com>
2024-05-06 19:45:08 -0700

    Adding styles to REST-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -6,6 +6,7 @@ function Def(html) {
     <html>
       <head>
         <title>Title</title>
+        <link rel="stylesheet" href="/style.css"/>
       </head>
       <body>{html.children}</body>
     </html>
