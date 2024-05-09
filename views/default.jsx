ae96cf39067f1bbd98ccc74a7d87986ace9d91de
JesusMLares <twinlares2@hotmail.com>
2024-05-08 20:22:11 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -10,7 +10,22 @@ function Def(html) {
         {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous"></link> */}
         <link rel="stylesheet" href="/style.css"/>
       </head>
-      <body>{html.children}</body>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
+        {html.children}
+        </body>
     </html>
   );
 }
