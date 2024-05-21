c621cb1cc1849df64a5fcc2d4e33c346dc9f225b
JesusMLares <twinlares2@hotmail.com>
2024-05-20 17:28:00 -0700

    Created error messages

--- models/places.js
+++ models/places.js
@@ -1,18 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
 
 // Place Schema
-const placeSchema = new mongoose.Schema({
+const placeSchema = new Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic: { type: String, default: "../public/images/green.jpg" },
   cuisines: { type: String, required: true },
-  city: { type: String, default: 'Anytown' },
-  state: { type: String, default: 'USA' },
-  founded: Number
-})
+  city: { type: String, default: "Anytown" },
+  state: { type: String, default: "USA" },
+  founded: {
+    type: Number,
+    min: [1673, "Surely not that old!"],
+    max: [new Date().getFullYear(), "Hey, this year is in the future!"],
+  },
+});
 
-placeSchema.methods.showEstablished = function(){
-  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}!`
-}
+placeSchema.methods.showEstablished = function () {
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}!`;
+};
 
 // Place Model
-module.exports = mongoose.model('Place', placeSchema)
+module.exports = mongoose.model("Place", placeSchema);
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,11 +1,21 @@
 const React = require("react");
 const Def = require("../default");
 
-function new_form() {
+function new_form(data) {
+  let message = '';
+  if (data.message){
+    message = (
+      <h4 style={{ color: "red" }}>
+      {data.message}
+      </h4>
+    )
+  }
+
   return (
     <Def>
       <main>
         <h1>Add a New Place</h1>
+        {message}
         <form action="/places" method="post">
           <div className="form-columns">
             <label htmlFor="name">Place Name:</label>
@@ -17,19 +27,25 @@ function new_form() {
           </div>
           <div className="form-columns">
             <label htmlFor="city">City:</label>
-            <input id="city" name="city" />
+            <input id="city" name="city" type="string" />
           </div>
           <div className="form-columns">
             <label htmlFor="state">State:</label>
-            <input id="state" name="state" />
+            <input id="state" name="state" type="string" />
           </div>
           <div className="form-columns">
             <label htmlFor="cuisines">Cuisines:</label>
-            <input id="cuisines" name="cuisines" />
+            <input id="cuisines" name="cuisines" type="string" />
           </div>
           <div className="form-columns">
             <label htmlFor="founded">Founded Year</label>
-            <input className="form-control" id="founded" name="founded" />
+            <input
+              className="form-control"
+              type="number"
+              id="founded"
+              name="founded"
+              defaultValue={new Date().getFullYear()}
+            />
           </div>
 
           <input type="submit" value="Add Place" className="btn" required />
